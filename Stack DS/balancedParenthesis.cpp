3a0f7cc3cebe0a4650ae0dee684271e399ef4d2f
debashishsamantara <debashishsamantara1@gmail.com>
2021-05-23 11:04:55 +0530

    finished stack

---
+++ Stack DS/balancedParenthesis.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s){
+    int n = s.length();
+    stack<char> st;
+    bool ans = true;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '{' || s[i] == '(' || s[i] == '['){
+            st.push(s[i]);
+        }else if(s[i] == ')'){
+            if(!st.empty() && st.top() == '('){
+                st.pop();
+            }else{
+                ans = false;
+            }
+        }else if(s[i] == '}'){
+            if(!st.empty() && st.top() == '{'){
+                st.pop();
+            }else{
+                ans = false;
+            }
+        }else if(s[i] == ']'){
+            if(!st.empty() && st.top() == '['){
+                st.pop();
+            }else{
+                ans = false;
+            }
+        }
+    }
+    if(!st.empty()){
+        return false;
+    }else{
+        return ans;
+    }
+}
+
+int main(){
+    string s = "{([])}";
+    if(isValid(s)){
+        cout << "Valid String";
+    }else{
+        cout << "Invalid String";
+    }
+    return 0;
+}
