4695643fef70f06d9823bda239845b5be08e594e
debashishsamantara <debashishsamantara1@gmail.com>
2021-08-21 15:24:47 +0530

    completed heaps and started hashing

--- Deque DS/slidingWindowMax.cpp
+++ Deque DS/slidingWindowMax.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-  
+
 int main(){
     int n, k; cin >> n >> k;
     vector<int> a(n);
     for(auto &i : a)
         cin >> i;
-    //Method-1    
+    //Method-1
     // multiset<int, greater<int>> s;
     // vector<int> ans;
     // for(int i = 0; i < k; i++){
